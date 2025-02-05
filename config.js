33e75972654dbbd934720db375683ca43d22eb78
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2025-02-05 20:16:17 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+        usertoken: 'usertoken1',
+        nodes: [
+            { nodeId: 'nodeid(pubkey)1', hardwareId: 'hardwareid1', proxy: 'proxy1' },
+            { nodeId: 'nodeid(pubkey)2', hardwareId: 'hardwareid2', proxy: 'proxy2' },
+            { nodeId: 'nodeid(pubkey)3', hardwareId: 'hardwareid3', proxy: 'proxy3' },
+            { nodeId: 'nodeid(pubkey)4', hardwareId: 'hardwareid4', proxy: 'proxy4' },
+            { nodeId: 'nodeid(pubkey)5', hardwareId: 'hardwareid5', proxy: 'proxy5' }
+        ]
+    },
+    {
+        usertoken: 'usertoken2',
+        nodes: [
+            { nodeId: 'nodeid(pubkey)6', hardwareId: 'hardwareid6', proxy: 'proxy6' }
+        ]
+    }
+    // Add more usertokens as needed
+];
